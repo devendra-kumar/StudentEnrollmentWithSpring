03f691bd5b2810bb85f5b9488007c1db2b4f1df7
Devendra Kumar <devendra.kumar157@gmail.com>
2017-03-23 21:46:06 +0530

    Updated dropdown binding
    Student detail link changes

--- src/main/java/com/github/elizabetht/controller/StudentController.java
+++ src/main/java/com/github/elizabetht/controller/StudentController.java
@@ -1,5 +1,9 @@
 package com.github.elizabetht.controller;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +13,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.github.elizabetht.model.Student;
 import com.github.elizabetht.model.StudentLogin;
@@ -64,4 +70,31 @@ public class StudentController {
 		}
 		
 	}
+	
+	@RequestMapping(value="/studentDetail", method=RequestMethod.GET)
+	public ModelAndView getStudentDetail(Model model) {
+		Map<Long, String> lst = new LinkedHashMap<Long, String>();
+		
+		List<Student> lstStudent = studentService.getAllActiveStudent();
+		
+		for (Student student : lstStudent) {
+			lst.put(student.getId(), student.toString());
+		}
+		
+		Student student = new Student();		
+		// This is mandatory as one of the attribute
+		model.addAttribute("student", student);
+		model.addAttribute("studentList", lst);		
+		return new ModelAndView("StudentDetail");
+	}
+	
+	@RequestMapping(value="/getStudentDetailByID")
+	public ModelAndView getStudentDetailByID(@RequestParam("id")long id)
+	{
+		ModelAndView mav = new ModelAndView("StudentInfo");
+		Student st = studentService.getStudentByID(id);
+		mav.addObject("stud", st);
+		return mav;
+	}
+	
 }
